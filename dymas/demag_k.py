Improve Kernel Math: - Fix sign error in off-diagonal elements of the kernel matrix.

dymas/demag_k.py
@@ -8,45 +8,51 @@ def Demag_Kernel_K(lz: int, dz: float, k: npt.NDArray,
                    dtype: str = 'complex128') -> npt.NDArray:
     '''
     Returns the demagnetization kernel K[i,a,j,b](k)
-    for a film discretized in lz parts, with height dz,
-    and for a wavevector kda
+    for an infinite film discretized in lz parts, with height dz,
+    and for a wavevector k
     '''
-
-    grid = np.mgrid[0:lz]
-    ZJ_Zi = dz*(grid[:, None] - grid[None, :])
-
+    k_theta = np.arctan2(k[1], k[0])
     abs_k = np.linalg.norm(k)
     kDz = abs_k*dz
-    sqr_k = abs_k**2
 
-    # Exponential times cosh term (only for Zj-Zi != 0)
-    exp_cosh = np.exp(-abs_k*np.abs(ZJ_Zi)) * (np.cosh(kDz) - 1) / kDz
+    # Calculate reusable terms
+    sinc_kdz = np.real(np.sinc(1.0j*kDz/(2*np.pi)))
+    exp_kdz = np.exp(-kDz/2)
 
-    # Exponential minus one over kDz term
-    exp_kDz = (np.exp(-kDz) - 1)/kDz
+    grid = np.mgrid[0:lz]
+    Zi_Zj = dz*(grid[None, :] - grid[:, None])
+    exp_kzizj = np.exp(-abs_k*np.abs(Zi_Zj))  # This is a matrix [ij]
 
     # allocate memory
     kernel = np.zeros((lz, 3, lz, 3), dtype=dtype)
     kernel_view = np.einsum('iajb->abij', kernel)
 
     # fill up the values
-    out_ab_common = exp_cosh / sqr_k
-    np.fill_diagonal(out_ab_common, (1+exp_kDz)/sqr_k)
-    kernel_view[0, 0] = k[0]*k[0] * out_ab_common
-    kernel_view[0, 1] = k[0]*k[1] * out_ab_common
-    kernel_view[1, 0] = k[1]*k[0] * out_ab_common
-    kernel_view[1, 1] = k[1]*k[1] * out_ab_common
 
-    out_a2_common = 1j * np.sign(ZJ_Zi) * exp_cosh / abs_k
-    np.fill_diagonal(out_a2_common, 0)
-    kernel_view[0, 2] = k[0] * out_a2_common
-    kernel_view[1, 2] = k[1] * out_a2_common
-    kernel_view[2, 0] = np.conjugate(kernel_view[0, 2])
-    kernel_view[2, 1] = np.conjugate(kernel_view[1, 2])
+    # Case a,b != 2 (x_a, x_b != z)
+    out_ab = -exp_kzizj * np.sinh(kDz/2) * sinc_kdz
+    np.fill_diagonal(out_ab, -(1-exp_kdz*sinc_kdz))  # i=j
+    kernel_view[0, 0] = np.cos(k_theta)**2 * out_ab
+    kernel_view[0, 1] = np.cos(k_theta) * np.sin(k_theta) * out_ab
+    kernel_view[1, 0] = np.sin(k_theta) * np.cos(k_theta) * out_ab
+    kernel_view[1, 1] = np.sin(k_theta)**2 * out_ab
+    # Note: Since out_ab == 0 for abs_k = 0 
+    # this equations provide the correct results for k=[0,0]
 
-    np.fill_diagonal(exp_cosh, exp_kDz)
-    kernel_view[2, 2] = -exp_cosh
-    return -kernel
+    # Case a xor b = 2
+    out_a2 = 1j * np.sign(Zi_Zj) * exp_kzizj * np.sinh(kDz/2) * sinc_kdz
+    np.fill_diagonal(out_a2, 0)  # i=j
+    kernel_view[0, 2] = kernel_view[2, 0] = np.cos(k_theta) * out_a2
+    kernel_view[1, 2] = kernel_view[2, 1] = np.sin(k_theta) * out_a2
+    # Note: Since out_a2 == 0 for abs_k = 0 
+    # this equations provide the correct results for k=[0,0]
+
+    # Case a and b = 2
+    out_22 = exp_kzizj * np.sinh(kDz/2) * sinc_kdz
+    np.fill_diagonal(out_22, -exp_kdz*sinc_kdz)  # i=j
+    kernel_view[2, 2] = out_22
+
+    return kernel
 
 
 def Demag_Kernel_K0(lz: int, dz: float,